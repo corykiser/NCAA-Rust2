adec648bca2a81aac9a76db90b8ca41d7aac7897
corykiser <45726562+corykiser@users.noreply.github.com>
2025-12-09 06:49:15 -0500

    Revert "Simulated Annealing Portfolio Optimization"

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ mod elo;
 mod api;
 mod game_result;
 mod portfolio;
-mod anneal;
 
 use clap::{Parser, ValueEnum};
 use rand::Rng;
@@ -27,8 +26,6 @@ enum PortfolioStrategy {
     Champion,
     /// Greedily maximize EV while penalizing similarity to previous brackets
     Diverse,
-    /// Optimize for maximum portfolio performance using simulated annealing
-    Annealing,
 }
 
 #[derive(Parser, Debug)]
@@ -95,10 +92,6 @@ struct Args {
     /// Can be specified multiple times
     #[arg(long)]
     lock_team: Vec<String>,
-
-    /// Number of simulations for annealing portfolio optimization
-    #[arg(long, default_value = "10000")]
-    annealing_sims: i32,
 }
 
 fn main() {
@@ -222,7 +215,6 @@ fn main() {
             &args.portfolio_strategy,
             &constraints,
             args.generations,
-            args.annealing_sims,
         );
     } else if !constraints.is_empty() {
         // Run single bracket with constraints
@@ -275,7 +267,6 @@ fn run_portfolio_mode(
     strategy: &PortfolioStrategy,
     constraints: &[BracketConstraint],
     generations: u32,
-    annealing_sims: i32,
 ) {
     println!();
     println!("=== Portfolio Mode ===");
@@ -300,14 +291,6 @@ fn run_portfolio_mode(
                 tournamentinfo,
                 num_brackets,
                 diversity_weight,
-                generations,
-            )
-        }
-        PortfolioStrategy::Annealing => {
-            BracketPortfolio::generate_annealed_portfolio(
-                tournamentinfo,
-                num_brackets,
-                annealing_sims,
                 generations,
             )
         }
